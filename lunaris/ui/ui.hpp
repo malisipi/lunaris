054cf6021715a5d9dcb968959a7566bfdd560662
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2024-09-25 23:57:02 +0300

    Draft on some event handlers

--- lunaris/ui/ui.hpp
+++ lunaris/ui/ui.hpp
@@ -7,6 +7,10 @@ namespace lunaris::ui {
         return ++last_registered_id;
     };
 
+    __uint128_t __get_event_time(){
+        return (__uint128_t)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+    }
+
     typedef struct widget {
         // Relative to layout
         int rx = 0;
@@ -85,10 +89,30 @@ namespace lunaris::ui {
             win->graphics.rect(this->fx, this->fy, this->fw, this->fh, 0xFFFFFFFF);
             win->graphics.text(this->fx, this->fy, std::min(this->fh, 20), this->text.c_str(), 0xFFFF0000);
         };
+        void(*click_handler)(lunaris::window*, lunaris::ui::widget*) = NULL;
+        void(*double_click_handler)(lunaris::window*, lunaris::ui::widget*) = NULL;
+        //std::chrono::time_point previous_click;
+        __uint128_t previous_click = 0;
         virtual void mouse_event(lunaris::window* win, float x, float y, bool pressed, float dx, float dy, lunaris::mouse::mouse event){
-            printf("Click at %f, %f\n", x, y);
-            if(pressed)
-                win->focused = (void*)this;
+            win->focused = (void*)this;
+            if(pressed){
+                    __uint128_t the_click = __get_event_time();
+                    __uint128_t elapsed_time = the_click-this->previous_click;
+                    if(elapsed_time>350){
+                        if(this->click_handler != NULL) this->click_handler(win, this);
+                        this->previous_click = the_click;
+                    } else {
+                        if(this->double_click_handler != NULL) this->double_click_handler(win, this);
+                        else if(this->click_handler != NULL) this->click_handler(win, this);
+                        this->previous_click = 0;
+                    };
+            };
+        };
+        virtual void keyboard_handler(lunaris::window* win, const char* new_char, lunaris::keycode::keycode key, uint32_t modifiers, lunaris::keyboard::keyboard event){
+            const int text_size = this->text.size();
+            if((event == keyboard::pressed || event == keyboard::repeated) && (key == keycode::enter || key == keycode::np_enter || key == keycode::space)){
+                this->mouse_event(win, 0, 0, 1, 0, 0, lunaris::mouse::first);
+            };
         };
     } button;
 
@@ -271,7 +295,7 @@ namespace lunaris::ui {
                     };
                 };
             };
-        }
+        };
     } textbox;
 
     const uint32_t fixed_id = request_new_id();
