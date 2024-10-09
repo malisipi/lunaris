3d308bc14c776e190a3ffb6a7581f0b1d8cfb89e
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2024-10-10 01:55:46 +0300

    Add keyboard handler and input_handler event for slider

--- lunaris/ui/ui.hpp
+++ lunaris/ui/ui.hpp
@@ -258,9 +258,11 @@ namespace lunaris::ui {
             win->graphics.rounded_rect(this->fx+10, this->fy+this->fh/2-4, this->fw-20, 8, 4, 0xFF444444);
             win->graphics.circle(this->fx+10+(this->value-this->min)*this->fw/(this->max-this->min+1), this->fy+this->fh/2, 10, 0xFFCCCCCC);
         };
+        void(*input_handler)(lunaris::window*, lunaris::ui::widget*, int prev_value) = NULL;
         virtual void mouse_event(lunaris::window* win, float x, float y, bool pressed, float dx, float dy, lunaris::mouse::mouse event){
             if(pressed && event == lunaris::mouse::first){
                 this->is_clicked = true;
+                win->focused = (void*)this;
             };
 
             if(this->is_clicked){
@@ -268,11 +270,15 @@ namespace lunaris::ui {
                 const int max_clickable_width = this->fw-20;
                 if(max_clickable_width<0 || this->min>=this->max) return;
                 int range = this->max-this->min+1;
+                int prev_value = this->value;
                 this->value = this->min + click_point*range/max_clickable_width;
                 if(this->value>this->max) this->value = this->max;
                 int fraction = (this->value-this->min)%this->step;
                 if(fraction != 0){ // TODO: Do it more accurate by rounding with `step` value
                     this->value -= fraction;
+                };
+                if(this->value != prev_value){
+                    if(this->input_handler) this->input_handler(win, this, prev_value);
                 };
                 if(!pressed && event != lunaris::mouse::motion){
                     this->is_clicked = false;
@@ -280,6 +286,17 @@ namespace lunaris::ui {
             }
         };
         virtual void keyboard_handler(lunaris::window* win, const char* new_char, lunaris::keycode::keycode key, uint32_t modifiers, lunaris::keyboard::keyboard event){
+            if(event == lunaris::keyboard::released) return;
+            
+            int prev_value = this->value;
+            if(key == lunaris::keycode::arrow_left){
+                this->value -= this->step;
+            } else if(key == lunaris::keycode::arrow_right){
+                this->value += this->step;
+            };
+            if(this->value<this->min || this->value>this->max){
+                this->value = prev_value;
+            } else if(this->input_handler) this->input_handler(win, this, prev_value);
         }
     } slider;
 
